99cc043688e687f4a11dde19e25d430d1a9f4a6e
sweedalgharshi <sweedalgracius@gmail.com>
2023-08-29 16:32:59 +0530

    added banner section

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,25 @@
 import "./Banner.scss";
+
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis molestiae esse,
+            minus illum atque perspiciatis voluptate
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img src={BannerImg} alt="banner-img" className="banner-img" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
