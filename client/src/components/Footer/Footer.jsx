d21fe4edc2fe9b6b2c55d3310d110aec8403066b
sweedalgharshi <sweedalgracius@gmail.com>
2023-09-02 16:41:40 +0530

    added footer section

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,64 @@
 import "./Footer.scss";
+
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus corporis officiis
+            excepturi iure culpa alias neque, a, suscipit est deserunt ut beatae. Inventore sequi
+            voluptatem nesciunt quis reiciendis.
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">
+              1234 Somewhere RoadNashville, TN 00000 United States of America
+            </div>
+          </div>
+          <div className="c-item">
+            <FaMobileAlt />
+            <div className="text">Phone: 011 4606 0956</div>
+          </div>
+          <div className="c-item">
+            <FaEnvelope />
+            <div className="text">Email: store@jsdev.com</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+          <span className="text">Smart Watches</span>
+          <span className="text">Bluetooth Speakers</span>
+          <span className="text">Wireless Earbuds</span>
+          <span className="text">Home Theatre</span>
+          <span className="text">Projectors</span>
+        </div>
+        <div className="col">
+          <div className="title">Pages</div>
+          <span className="text">Home</span>
+          <span className="text">About</span>
+          <span className="text">Privacy Policy</span>
+          <span className="text">Returns</span>
+          <span className="text">Terms & Conditions</span>
+          <span className="text">Contact Us</span>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">
+            JSDEVSTORE 2023 CREATED BY S. GRACIUS. PREMIUM E-COMMERCE SOLUTIONS.
+          </div>
+          <img src={Payment} alt="payment" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
