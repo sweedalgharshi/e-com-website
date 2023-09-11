e1b6fc675e3d2de8ca6c100d0042c397c46ca7e7
sweedalgharshi <sweedalgracius@gmail.com>
2023-09-11 16:57:03 +0530

    Product details page API integration

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,9 +1,11 @@
 import "./Product.scss";
+import { useNavigate } from "react-router-dom";
 
 // import prod from "../../../assets/products/earbuds-prod-1.webp";
 const Product = ({ id, data }) => {
+  const navigate = useNavigate();
   return (
-    <div className="product-card">
+    <div className="product-card" onClick={() => navigate(`/product/${id}`)}>
       <div className="thumbnail">
         <img
           src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url}
--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -11,26 +11,52 @@ import {
   FaCartPlus,
 } from "react-icons/fa";
 
-import prod from "../../assets/products/headphone-prod-1.webp";
+// import prod from "../../assets/products/headphone-prod-1.webp";
+
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const SingleProduct = () => {
+  const [quantity, setQuantity] = useState(1);
+  const { id } = useParams();
+  const { data } = useFetch(`/api/products?populate=*&[filters][id]=${id}`);
+
+  if (!data) return;
+  const product = data.data[0].attributes;
+
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+  const decrement = () => {
+    setQuantity((prevState) => {
+      if (prevState === 1) {
+        return 1;
+      } else {
+        return prevState - 1;
+      }
+    });
+  };
   return (
     <div className="single-product-main-content">
       <div className="layout">
         <div className="single-product-page">
           <div className="left">
-            <img src={prod} alt="product" />
+            <img
+              src={process.env.REACT_APP_DEV_URL + product.img.data[0].attributes.url}
+              alt="product"
+            />
           </div>
           <div className="right">
-            <span className="name">Product Name</span>
-            <span className="price">Product Price</span>
-            <span className="desc">Product Description</span>
+            <span className="name">{product.title}</span>
+            <span className="price">&#8377;{product.price}</span>
+            <span className="desc">{product.desc}</span>
 
             <div className="cart-buttons">
               <div className="quantity-buttons">
-                <span>-</span>
-                <span>2</span>
-                <span>+</span>
+                <span onClick={decrement}>-</span>
+                <span>{quantity}</span>
+                <span onClick={increment}>+</span>
               </div>
               <button className="add-to-cart-button">
                 <FaCartPlus size={20} />
@@ -42,8 +68,7 @@ const SingleProduct = () => {
 
             <div className="info-item">
               <span className="text-bold">
-                Category:
-                <span>Headphones</span>
+                Category: <span>{product.categories.data[0].attributes.title}</span>
               </span>
 
               <span className="text-bold">
