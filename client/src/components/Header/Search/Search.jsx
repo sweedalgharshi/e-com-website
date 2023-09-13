8923a21fe17a153d956f5ecd37c0c0ff0a9c5c4a
sweedalgharshi <sweedalgracius@gmail.com>
2023-09-13 16:30:30 +0530

    live search functionality

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,25 +1,60 @@
 import "./Search.scss";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+
 import { MdClose } from "react-icons/md";
-import prod from "../../../assets/products/speaker-prod-1.webp";
+// import prod from "../../../assets/products/speaker-prod-1.webp";
 const Search = ({ setShowSearch }) => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  let { data } = useFetch(`/api/products?populate=*&filters[title][$contains]=${query}`);
+
+  if (!query.length) {
+    data = null;
+  }
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for product" />
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for product"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+            // console.log(query);
+          }}
+        />
         <MdClose onClick={() => setShowSearch((showSearch) => !showSearch)} className="close-btn" />
       </div>
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="product-img" />
-            </div>
-            <div className="product-details">
-              <span className="name">product name</span>
-              <span className="desc">product description</span>
-            </div>
-          </div>
+          {data?.data?.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="search-result-item"
+                onClick={() => {
+                  navigate(`/product/${item.id}`);
+                  setShowSearch(false);
+                }}
+              >
+                <div className="img-container">
+                  <img
+                    src={process.env.REACT_APP_DEV_URL + item.attributes.img.data[0].attributes.url}
+                    alt="product-img"
+                  />
+                </div>
+                <div className="product-details">
+                  <span className="name">{item.attributes.title}</span>
+                  <span className="desc">{item.attributes.desc}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
